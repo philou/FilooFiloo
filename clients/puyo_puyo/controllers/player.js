152cd6ef7e2e5d140b882d1ef92e328d3be6641f
Philippe <philippe.bourgau@gmail.com>
2008-10-05 23:04:34 +0200

    First files.

---
+++ clients/puyo_puyo/controllers/player.js
@@ -0,0 +1,23 @@
+// ==========================================================================
+// PuyoPuyo.PlayerController
+// ==========================================================================
+
+require('core');
+
+/** @class
+
+  @extends SC.Object
+  @author Philou
+  @version 0.1
+  @static
+*/
+PuyoPuyo.playerController = SC.Object.create(
+/** @scope PuyoPuyo.playerController */ {
+
+    start: function() {
+	this.get('board').start();
+    },
+    stop: function() {
+	this.get('board').stop();
+    }
+}) ;
