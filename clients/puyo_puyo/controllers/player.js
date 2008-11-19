bc42e4795d4e15c582fdad872df4fc44d7bf00d4
Philippe <philippe.bourgau@gmail.com>
2008-11-19 22:06:45 +0100

    game over panel

--- clients/puyo_puyo/controllers/player.js
+++ clients/puyo_puyo/controllers/player.js
@@ -17,7 +17,18 @@ PuyoPuyo.playerController = SC.Object.create(
     start: function() {
 	this.get('board').start();
     },
+
     abort: function() {
 	this.get('board').abort();
+    },
+
+    openGameOverDialog: function() {
+        if (this.get('board').get('gameOver'))
+            SC.page.get('gameOverPane').set('isVisible', YES);
+    }.observes('.board.gameOver'),
+
+    closeGameOverDialog: function() {
+        SC.page.get('gameOverPane').set('isVisible', NO);
     }
+    
 }) ;
