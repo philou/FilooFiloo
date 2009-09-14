eb53ffbc481447197ad9b74c6d5dbb5207f5346f
Philou <philou@eeepc-linux.(none)>
2009-09-14 23:50:35 +0200

    tests for player controller

--- clients/filoo_filoo/controllers/player.js
+++ clients/filoo_filoo/controllers/player.js
@@ -7,82 +7,99 @@ require('models/player');
 
 /** @class
 
-  Represents the current player's attributes
+ Represents the current player's attributes
 
-  @extends SC.Object
-  @author AuthorName
-  @version 0.1
-  @static
-*/
-FilooFiloo.playerController = SC.Object.create(
-/** @scope FilooFiloo.playerController */ {
+ @extends SC.Object
+ @author AuthorName
+ @version 0.1
+ @static
+ */
+FilooFiloo.createPlayerController = function() {
+  return SC.Object.create(
+    /** @scope FilooFiloo.playerController */ {
 
-  /* current mode (single, versus ...)
-   * corresponds to the current main tab
-   */
-  currentMode: undefined,
+      /* current mode (single, versus ...)
+       * corresponds to the current main tab
+       */
+      currentMode: undefined,
 
-  /* Name of the current player
-   */
-  name: undefined,
+      /* Name of the current player
+       */
+      name: undefined,
 
-  /*
-   * Text in the login pane
-   */
-  loginTitle: 'Login',
-  loginCaption: 'Please choose a surname.',
-  _doAfterLogin: null,
+      /*
+       * Content of the login pane
+       */
+      loginPaneVisible: NO,
+      loginTextRequired: YES,
 
-  /*
-   * What is the player doing now ?
-   */
-  whatIsPlayerDoing: 'nope',
+      /*
+       * Text in the login pane
+       */
+      loginTitle: 'Login',
+      loginCaption: 'Please choose a surname.',
+      _doAfterLogin: null,
 
-  forceLoginAndDo: function(loginTitle, loginCaption, doAfterLogin) {
-    this.set('loginTitle', loginTitle);
-    this.set('loginCaption', loginCaption);
-    this._doAfterLogin = doAfterLogin;
-    var gameOverPane = SC.page.get('loginPane');
-    gameOverPane.get('requiredLoginText').set('isVisible', !this.get('name'));
-    gameOverPane.set('isVisible', YES);
-  },
+      /*
+       * What is the player doing now ?
+       */
+      whatIsPlayerDoing: 'nothing',
 
-  closeLoginPane: function() {
-    if (this.get('name')) {
-      SC.page.get('loginPane').set('isVisible', NO);
-      this._doAfterLogin(this.get('name'));
+      /*
+       * External refs
+       */
+      Timer: SC.Timer,
+      Player: FilooFiloo.Player,
+
+      forceLoginAndDo: function(loginTitle, loginCaption, doAfterLogin) {
+	this.set('loginTitle', loginTitle);
+	this.set('loginCaption', loginCaption);
+	this._doAfterLogin = doAfterLogin;
+	this.set('loginTextRequired', !this.get('name')); // this property is not bound to name dynamicaly, otherwise the
+        // text box would disappear after the first entered letter
+	this.set('loginPaneVisible', YES);
+      },
+
+      closeLoginPane: function() {
+	if (this.get('name')) {
+	  this.set('loginPaneVisible', NO);
+	  this._doAfterLogin(this.get('name'));
+	}
+      },
+
+      requestNameForVersusMode: function() {
+        if ('versus' === this.get('currentMode')) {
+	  if(!this.get('name')) {
+	    this.forceLoginAndDo('Login', 'Filoo Filoo rules... you need to login in order to play against someone.',
+				 this._startWaitingForOpponent);
+	  } else {
+	    this._startWaitingForOpponent();
+	  }
+	}
+      }.observes('currentMode'),
+
+      _startWaitingForOpponent: function() {
+	this.set('whatIsPlayerDoing', 'Waiting for an opponent ...');
+
+	this.player = this.Player.newRecord({name: this.name}, FilooFiloo.server);
+	this.player.commit();
+
+	this.ticks = 0;
+	this.timer = this.Timer.schedule({target: this, action: '_checkForOpponent', repeats: YES, interval: 1000});
+      },
+
+      _checkForOpponent: function() {
+	this.player.refresh();
+	if (this.player.get('opponentName')) {
+	  this.set('whatIsPlayerDoing', 'Playing against '+this.player.get('opponentName'));
+	  this.timer.invalidate();
+	} else {
+	  this.ticks = this.ticks + 1;
+	  this.set('whatIsPlayerDoing', 'Waiting for an opponent ... '+this.ticks+' seconds');
+	}
+      }
     }
-  },
+  );
+};
 
-  requestNameForVersusMode: function() {
-    if( !this.get('name') && ('versus' === this.get('currentMode'))) {
-      this.forceLoginAndDo('Login', 'Filoo Filoo rules... you need to login in order to play against someone.',
-			   this.startWaitingForOpponent);
-    } else {
-      this.startWaitingForOpponent();
-    }
-  }.observes('currentMode'),
-
-  startWaitingForOpponent: function() {
-    this.set('whatIsPlayerDoing', 'Waiting for an opponent ...');
-
-    this.player = FilooFiloo.Player.newRecord({name: this.name}, FilooFiloo.server);
-    this.player.commit();
-
-    this.ticks = 0;
-    this.timer = SC.Timer.schedule({
-      target: this, action: 'checkForOpponent', repeats: YES, interval: 1000});
-  },
-
-  checkForOpponent: function() {
-    this.player.refresh();
-    if (this.player.get('opponentName')) {
-      this.set('whatIsPlayerDoing', 'Playing against '+this.player.get('opponentName'));
-      this.timer.invalidate();
-    } else {
-      this.ticks = this.ticks + 1;
-      this.set('whatIsPlayerDoing', 'Waiting for an opponent ... '+this.ticks+' seconds');
-    }
-  }
-
-}) ;
+FilooFiloo.playerController = FilooFiloo.createPlayerController();
