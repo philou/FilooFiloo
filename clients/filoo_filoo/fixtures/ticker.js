1a25ef612653cef7258a7140fbd1e3dbd86a62b1
Philou <philou@eeepc-linux.(none)>
2009-09-17 00:22:33 +0200

    Added MIT licence

--- clients/filoo_filoo/fixtures/ticker.js
+++ clients/filoo_filoo/fixtures/ticker.js
@@ -1,3 +1,17 @@
+// Copyright (c) 2008-2009  Philippe Bourgau
+
+// This program is free software: you can redistribute it and/or modify
+// it under the terms of the MIT License.
+
+// This program is distributed in the hope that it will be useful, but
+// WITHOUT ANY WARRANTY; without even the implied warranty of
+// MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the MIT
+// License for more details.
+
+// You should have received a copy of the MIT License along with this
+// program.  If not, see <http://www.opensource.org/licenses/mit-license.php>
+
+
 // ==========================================================================
 // FilooFiloo.Ticker Fixtures
 // ==========================================================================
