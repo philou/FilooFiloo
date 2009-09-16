83d02fbd2028af248ede8bccde24ed902f6f940a
Philou <philou@eeepc-linux.(none)>
2009-09-17 00:25:59 +0200

    Added MIT Licence

--- server/filoo_filoo/config/initializers/new_rails_defaults.rb
+++ server/filoo_filoo/config/initializers/new_rails_defaults.rb
@@ -1,3 +1,16 @@
+# Copyright (c) 2008-2009  Philippe Bourgau
+
+# This program is free software: you can redistribute it and/or modify
+# it under the terms of the MIT License.
+
+# This program is distributed in the hope that it will be useful, but
+# WITHOUT ANY WARRANTY; without even the implied warranty of
+# MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the MIT
+# License for more details.
+
+# You should have received a copy of the MIT License along with this
+# program.  If not, see <http://www.opensource.org/licenses/mit-license.php>
+
 # These settings change the behavior of Rails 2 apps and will be defaults
 # for Rails 3. You can remove this initializer when Rails 3 is released.
 
