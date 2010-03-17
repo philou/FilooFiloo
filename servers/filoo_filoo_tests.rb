e0386fa92678776497e34b2e65d1b031bfb632d9
philou <philippe.bourgau@free.fr>
2010-03-17 07:57:36 +0100

    Players can be saved and put in opposed.

--- servers/filoo_filoo_tests.rb
+++ servers/filoo_filoo_tests.rb
@@ -2,10 +2,7 @@
 
 require 'fileutils'
 
-# better to start from a empty test database
-DB_FILE = "/tmp/filoo_filoo_tests.db"
-ENV['DATABASE_URL'] = "sqlite3://#{DB_FILE}"
-FileUtils.rm_f(DB_FILE)
+ENV['DATABASE_URL'] = "sqlite3://memory:"
 
 require 'filoo_filoo'
 require 'test/unit'
@@ -14,46 +11,123 @@ require 'json'
 
 set :environment, :test
 
-class HighScoreTest < Test::Unit::TestCase
+module FilooFilooTestMethods
   include Rack::Test::Methods
 
-  def test_ruby_to_json_to_ruby_should_look_alike
-    [{:player_name => "Philou", :score => 666},
-     {:player_name => "AC", :score => 123}].each { |score|
+  def setup
+    @__transaction = DataMapper::Transaction.new(DataMapper.repository(:default))
+    @__transaction.begin
 
-      converted = HighScore.parse_json(HighScore.new(score).to_json)
+    # FIXME: Should I really be calling #push_transaction like that, or is there a better way?
+    DataMapper.repository(:default).adapter.push_transaction(@__transaction)
+  end
 
-      assert_equal score[:player_name], converted[:player_name]
-      assert_equal score[:score], converted[:score]
-    }
+  def teardown
+    if @__transaction
+      DataMapper.repository(:default).adapter.pop_transaction
+      @__transaction.rollback
+      @__transaction = nil
+    end
+  end
+
+  def test_json_roundtrip(fixtures, klass)
+    fixtures.each do |attributes|
+      converted = klass.parse_json(klass.new(attributes).to_json)
+      attributes.each { |key, value| assert_equal value, converted[key] }
+    end
   end
 
   def app
     Sinatra::Application
   end
 
-  def test_all_scores_should_have_a_content
-    get '/high_scores'
+  def test_post(url, json, location_regex)
+    post url, json
+    assert_equal 201, last_response.status
+    assert last_response.headers.has_key?("Location")
+    location = last_response.headers["Location"]
+    assert location =~ location_regex
+    location
+  end
+
+  def test_get(url)
+    get url
     assert last_response.ok?
-    scores = JSON.parse(last_response.body)
-    assert scores["content"]
+    body = JSON.parse(last_response.body)
+    assert body["content"]
+    body["content"]
+  end
+
+end
+
+class HighScoreTest  < Test::Unit::TestCase
+  include FilooFilooTestMethods
+
+  def test_ruby_to_json_to_ruby_should_look_alike
+    test_json_roundtrip([{:player_name => "Philou", :score => 666},
+                         {:player_name => "AC", :score => 123}], HighScore)
+  end
+
+  def test_all_scores_should_have_a_content
+    test_get '/high_scores'
   end
 
   def test_posting_a_score_should_ok
-    post '/high_scores', '{"playerName":"Philou","score":7}'
-    assert_equal 201, last_response.status
-    assert last_response.headers.has_key?("Location")
+    get '/high_scores'
+    assert_equal 0, JSON.parse(last_response.body)["content"].length
+
+    test_post '/high_scores', '{"playerName":"Philou","score":7}', /\/high_scores\/[0-9]+/
   end
 
   def test_a_posted_scores_should_appear_in_all_scores
     get '/high_scores'
-    assert_equal 0, JSON.parse(last_response.body)["content"].length
+    scoresCount = JSON.parse(last_response.body)["content"].length
 
     post '/high_scores', '{ "playerName": "AC", "score":666 }'
     post '/high_scores', '{ "playerName": "DC", "score":667 }'
 
     get '/high_scores'
-    assert_equal 2, JSON.parse(last_response.body)["content"].length
+    assert_equal scoresCount + 2, JSON.parse(last_response.body)["content"].length
+  end
+
+end
+
+class PlayerTest < Test::Unit::TestCase
+  include FilooFilooTestMethods
+
+  def test_ruby_to_json_to_ruby_should_look_alike
+    test_json_roundtrip([{:name => "Philou",},
+                         {:name => "AC", :opponent_name => "Philou", :board_string => " br "}], Player)
+  end
+
+  def test_create_player(name="Philou")
+    test_post '/players', "{\"name\": \"#{name}\"}", /\/players\/[0-9]+/
+  end
+
+  def test_create_and_get_player(name="AC")
+    location = test_create_player(name)
+    player_json = test_get location
+    assert_equal name, player_json["name"]
+    player_json
+  end
+
+  def _test_put_player_should_ok
+  end
+
+  def test_a_lonely_player_should_not_get_an_opponent
+    player_json = test_create_and_get_player("Donald")
+    assert player_json["opponentName"].nil?
+  end
+
+  def test_two_players_should_play_against_each_other
+    bonnyUrl = test_create_player("Bonny")
+    clide = test_create_and_get_player("Clide")
+
+    assert_equal "Bonny", clide["opponentName"]
+    assert_equal "Clide", (test_get bonnyUrl)["opponentName"]
+  end
+  
+  def _test_grid_of_a_player_should_be_updatable
   end
 
 end
