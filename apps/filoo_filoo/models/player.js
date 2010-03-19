1e35d08419d4ae3a5dfa2a83758a806ce2bda1b6
philou <philippe.bourgau@free.fr>
2010-03-19 06:42:35 +0100

    Tests for versus controller are working

---
+++ apps/filoo_filoo/models/player.js
@@ -0,0 +1,35 @@
+// Copyright (c) 2008-2010  Philippe Bourgau
+
+// This program is free software: you can redistribute it and/or modify
+// it under the terms of the GNU Affero General Public License as
+// published by the Free Software Foundation, either version 3 of the
+// License, or (at your option) any later version.
+
+// This program is distributed in the hope that it will be useful, but
+// WITHOUT ANY WARRANTY; without even the implied warranty of
+// MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+// GNU Affero General Public License for more details.
+
+// You should have received a copy of the GNU Affero General Public License
+// program.  If not, see <http://www.gnu.org/licenses/>
+
+
+// ==========================================================================
+// FilooFiloo.Player
+// ==========================================================================
+
+/** @class
+
+  (Document your class here)
+
+  @extends SC.Record
+  @author AuthorName
+  @version 0.1
+*/
+FilooFiloo.Player = SC.Record.extend(
+/** @scope FilooFiloo.Player.prototype */ {
+
+  playerName: SC.Record.attr(String),
+  opponentName: SC.Record.attr(String),
+  boardString: SC.Record.attr(String)
+}) ;
---
+++ apps/filoo_filoo/resources/versus_page.js
@@ -0,0 +1,60 @@
+// Copyright (c) 2008-2010  Philippe Bourgau
+
+// This program is free software: you can redistribute it and/or modify
+// it under the terms of the GNU Affero General Public License as
+// published by the Free Software Foundation, either version 3 of the
+// License, or (at your option) any later version.
+
+// This program is distributed in the hope that it will be useful, but
+// WITHOUT ANY WARRANTY; without even the implied warranty of
+// MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+// GNU Affero General Public License for more details.
+
+// You should have received a copy of the GNU Affero General Public License
+// program.  If not, see <http://www.gnu.org/licenses/>
+
+
+FilooFiloo.versusPage = SC.Page.design(
+{
+  mainView: SC.View.design(
+  {
+    classNames: ['main-view'],
+    layout: FilooFiloo.Layout.MAIN_VIEW,
+    childViews:
+    [
+      SC.LabelView.design(
+      {
+	layout: FilooFiloo.Layout.scoreRow(0),
+	value: 'YOU'
+      }),
+      SC.LabelView.design(
+      {
+	layout: FilooFiloo.Layout.scoreRow(1),
+	valueBinding: 'FilooFiloo.versusController.whatIsPlayerDoing'
+      }),
+      FilooFiloo.BoardView.design(
+      {
+	layout: { left: 1, right: 1, top: FilooFiloo.Layout.scoreRowTop(2), height: 380 },
+	contentBinding: 'FilooFiloo.versusController.board'
+      })
+    ]
+  })
+});
+
+/*
+<% view :versus_tab do %>
+
+<div class="versus">
+  <p><%= label_view :bind => { :value => 'FilooFiloo.versusController.whatIsPlayerDoing' } %></p>
+  <div id="boards">
+    <div id="yourArea">
+      <%= view :yourBoardView, :tag => :table, :class => 'board', :view => 'FilooFiloo.BoardView',
+        :bind => { :board => 'FilooFiloo.versusController.board' } %>
+      <p>YOU</p>
+    </div>
+  </div>
+</div>
+<% end %>
+*/
+
+
