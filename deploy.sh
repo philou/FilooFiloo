53336c29de816198539de58d4694cc805e30a319
Philou <philippe.bourgau@free.fr>
2010-05-07 07:34:48 +0200

    Updates deploy script

--- deploy.sh
+++ deploy.sh
@@ -1,7 +1,8 @@
 #!/bin/sh
 
-git rm -r tmp/build
 sc-build --languages=en --build=LATEST --clean
+git rm -r public/static
+cp -r tmp/build/static public/
 git add .
 git commit -m "Deployed `date`"
 git push origin production
