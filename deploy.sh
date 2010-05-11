2c2655ae5cdd201b4e593832f5041fb87d58cb6f
Philou <philippe.bourgau@free.fr>
2010-05-11 07:14:49 +0200

    Improve error report on server

--- deploy.sh
+++ deploy.sh
@@ -5,8 +5,8 @@ git merge master
 sc-build --languages=en --build=LATEST --clean
 git rm -r public/static
 cp -r tmp/build/static public/
-git add .
-git commit -m "Deploys to heroku"
+git add public/.
+git commit -m "Deploy to heroku"
 git push origin production
 git push heroku production:master
 git checkout master
